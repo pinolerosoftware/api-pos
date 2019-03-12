0c6bf365906fffd19fc8d0f255bac3c02469795b
Johan Godinez <johang77@hotmail.com>
2019-03-11 23:16:40 -0600

    Quitando el motor de plantillas para trabajar unicamente con json

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const createError = require('http-errors');
 const cors = require('cors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const app = express();
 
 
 const indexRouter = require('./routes/index');
@@ -13,41 +13,28 @@ const locationsRouter = require('./routes/locations');
 const categoriesRouter = require('./routes/categories');
 const companiesRouter = require('./routes/companies');
 
-const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Routers
-app.use('/', indexRouter);
+app.use('/api/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/locations', locationsRouter);
 app.use('/api/categories', categoriesRouter);
 app.use('/api/companies', companiesRouter);
 
-app.use('/login', (req, res) => {
-  res.render('login');
-})
-
-app.use('/products', (req, res) => {
-  res.render('products');
-})
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -57,7 +44,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({error: res.locals.error});
 });
 
 module.exports = app;
