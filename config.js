5b3a8769acb80cc3b88164eeb11e7b1f07939f28
Johan Godinez <johang77@hotmail.com>
2019-06-23 23:56:49 -0600

    Remove config to gititnore

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    port: process.env.PORT || 3001,
+    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/test',
+    SECRET_TOKEN: "*J}Kx1rn44",
+    ACCESSKEYID: "AKIAITXRM4QE6EROHITA",
+    SECRETACCESSKEY: "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",
+    BUCKETNAME: "api-pos",
+}
